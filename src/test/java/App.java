907a85621a189a932505cd33abb1ee0b069a2e27
Ahmed.Shire <ahmed.shire@ten10.com>
2023-10-13 10:27:35 +0100

    test

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -2,6 +2,8 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
+import java.nio.file.Paths;
+
 public class App {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
@@ -9,6 +11,12 @@ public class App {
             Page page = browser.newPage();
             page.navigate("http://playwright.dev");
             System.out.println(page.title());
+        }
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.webkit().launch();
+            Page page = browser.newPage();
+            page.navigate("https://www.demoblaze.com/index.html");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshot.png")));
         }
     }
 }
